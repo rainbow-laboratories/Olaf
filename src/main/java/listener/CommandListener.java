f641bf64fbfcc52bebf817d2fd38c28e156623fc
Stoffel <stoffel2107@gmail.com>
2018-10-20 00:22:15 +0200

    Persilas der Seelachs is now talking
    Signed-off-by: Stoffel <stoffel2107@gmail.com>

--- src/main/java/listener/CommandListener.java
+++ src/main/java/listener/CommandListener.java
@@ -9,6 +9,10 @@ public class CommandListener extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         if(event.getMessage().getContentRaw().startsWith(variables.prefix) && event.getMessage().getAuthor().getId() != event.getJDA().getSelfUser().getId()) {
             CommandHandler.handleCommand(CommandHandler.parser.parse(event.getMessage().getContentRaw(), event));
+        } else if (event.getAuthor().getName().matches("Silas770")) {
+            System.out.println(event.getAuthor().getName());
+            event.getMessage().delete().queue();
+            event.getTextChannel().sendMessage("Persilas der Seelachs sagt: " + event.getMessage().getContentRaw()).queue();
         }
     }
 }
