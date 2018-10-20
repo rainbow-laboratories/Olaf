c327c1361f265f1e07e0321e2a0dfa4c9ed2d4c9
Stoffel <stoffel2107@gmail.com>
2018-10-20 17:15:21 +0200

    Olaf now reacts to the same commands when he is mentioned and the prefix isn't used needs improvement though Signed-off-by: Stoffel <stoffel2107@gmail.com>

--- src/main/java/listener/CommandListener.java
+++ src/main/java/listener/CommandListener.java
@@ -11,12 +11,18 @@ import utils.variables;
  */
 public class CommandListener extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
-        if(event.getMessage().getContentRaw().startsWith(variables.prefix) && event.getMessage().getAuthor().getId() != event.getJDA().getSelfUser().getId()) {
+        if((event.getMessage().getContentRaw().startsWith(variables.prefix) && event.getMessage().getAuthor().getId() != event.getJDA().getSelfUser().getId())) {
             CommandHandler.handleCommand(CommandHandler.parser.parse(event.getMessage().getContentRaw(), event));
         } else if (event.getAuthor().getName().matches("Silas770")) {
             System.out.println(event.getAuthor().getName());
             event.getMessage().delete().queue();
             event.getTextChannel().sendMessage("Persilas der Seelachs sagt: " + event.getMessage().getContentRaw()).queue();
+        } else if(event.getMessage().getMentionedMembers().size() > 0) {
+            if(event.getMessage().getMentionedMembers().get(0).getUser().equals(event.getJDA().getSelfUser())) {
+                String tempString = event.getMessage().getContentRaw();
+                String newString = tempString.replace("<@" + event.getJDA().getSelfUser().getId() + "> ", variables.prefix);
+                CommandHandler.handleCommand(CommandHandler.parser.parse(newString, event));
+            }
         }
     }
 }
